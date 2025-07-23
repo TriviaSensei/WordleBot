72350864609ddd4f93fbddbddd619735a668770d
Chuck <charles.yu4@gmail.com>
2025-07-22 22:16:39 -0400

    removing console.logs

--- utils/wordleBot/wordleBot.js
+++ utils/wordleBot/wordleBot.js
@@ -508,8 +508,6 @@ const checkCorrectChannel = async (srvr, msg, ...suppressWarnings) => {
 };
 
 const updateServerData = async (guildId) => {
-	console.log(`Update server data ${guildId}`);
-	console.trace();
 	//this should not happen - if the server's guild ID was not found for some reason, just return an error
 	const serverData = (await axios.get(`${url}/guilds/${guildId}`, authObj))
 		?.data;
