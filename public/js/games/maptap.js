b78e59351a525da0f11c9a5e20d9ac087998066e
Chuck <charles.yu4@gmail.com>
2026-06-29 10:25:53 -0400

    color scale for maptap

--- public/js/games/maptap.js
+++ public/js/games/maptap.js
@@ -134,11 +134,9 @@ const updateData = (e) => {
 			defaultSort: -1,
 			finally: (cell) => {
 				const data = Number(cell.getAttribute('data-value'));
-				console.log(data);
-				if (!isNaN(data)) return;
+				if (isNaN(data)) return;
 				let obj = colorScales.find((el) => data <= el.max && data >= el.min);
 				if (!obj) obj = colorScales[colorScales.length - 1];
-				console.log(obj);
 				cell.setAttribute('style', `background-color: ${obj.fn(data)}`);
 			},
 		},
